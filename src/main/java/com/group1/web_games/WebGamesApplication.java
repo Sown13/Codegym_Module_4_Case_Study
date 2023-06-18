705c4a473833cd88ec37bdbf6b1aba70ba642c9a
Sown13 <sonnh.buh@gmail.com>
2023-06-18 16:57:20 +0700

    first init

---
+++ src/main/java/com/group1/web_games/WebGamesApplication.java
@@ -0,0 +1,13 @@
+package com.group1.web_games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebGamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebGamesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group1/web_games/WebGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group1.web_games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebGamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
