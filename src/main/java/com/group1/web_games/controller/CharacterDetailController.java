668a92cac0e8468eb5d2fd0129cac6ee7bdad3d4
giangthidua <125744426+giangadua06042001@users.noreply.github.com>
2023-06-20 20:41:37 +0700

    Xong class CharacterDetail

---
+++ src/main/java/com/group1/web_games/controller/CharacterDetailController.java
@@ -0,0 +1,44 @@
+package com.group1.web_games.controller;
+
+import com.group1.web_games.model.main_entity.CharacterDetail;
+import com.group1.web_games.service.character_detail.ICharacterDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/character-detail-controller")
+public class CharacterDetailController {
+    @Autowired
+    private ICharacterDetailService characterDetailService;
+    @PostMapping
+    private ResponseEntity<CharacterDetail>saveCharacterDetail(@RequestBody CharacterDetail characterDetail){
+        return new ResponseEntity<>(characterDetailService.save(characterDetail), HttpStatus.OK);
+    }
+    @GetMapping
+    private ResponseEntity<Iterable<CharacterDetail>>findAllCharacterDetail(){
+        return new ResponseEntity<>(characterDetailService.findAll(),HttpStatus.OK);
+    }
+    @DeleteMapping("{id}")
+    private ResponseEntity<CharacterDetail>deleteCharacterDetailById(@PathVariable Long id){
+        Optional<CharacterDetail>characterDetail=characterDetailService.findById(id);
+        if(!characterDetail.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        characterDetailService.remove(id);
+        return new ResponseEntity<>(characterDetail.get(),HttpStatus.OK);
+    }
+    @PutMapping("{id}")
+    private ResponseEntity<CharacterDetail>updateCharacterDetail(@PathVariable Long id){
+        Optional<CharacterDetail>characterDetail=characterDetailService.findById(id);
+        if(!characterDetail.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        characterDetailService.save(characterDetail.orElse(null));
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/group1/web_games/service/character_detail/ICharacterDetailService.java
@@ -0,0 +1,9 @@
+package com.group1.web_games.service.character_detail;
+
+import com.group1.web_games.model.main_entity.CharacterDetail;
+import com.group1.web_games.service.IGeneralService;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ICharacterDetailService extends IGeneralService<CharacterDetail> {
+}
