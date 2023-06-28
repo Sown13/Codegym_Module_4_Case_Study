3befd9873408b865c407074f2f3ccbbe86b7ed59
Tuong <aumanhtuong09122001@gmail.com>
2023-06-28 19:06:21 +0700

    login xong

--- src/main/java/com/group1/web_games/controller/main_controller/UserEntityRestController.java
+++ src/main/java/com/group1/web_games/controller/main_controller/UserEntityRestController.java
@@ -1,12 +1,15 @@
 package com.group1.web_games.controller.main_controller;
 
 import com.group1.web_games.model.main_entity.UserEntity;
+import com.group1.web_games.model.user.User;
 import com.group1.web_games.service.main_entity_service.user_entity.IUserEntityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -40,5 +43,33 @@ public class UserEntityRestController {
         }
         userEntityService.save(userEntity.orElse(null));
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerUser(@RequestBody UserEntity userEntity) {
+        if (userEntityService.findByUserEmail(userEntity.getEmail()) != null) {
+            return ResponseEntity.badRequest().body("Email already exists");
+        } else {
+            userEntityService.save(userEntity);
+            return ResponseEntity.ok().body("User register successfully");
+        }
+    }
+    @PostMapping("/login")
+    public ResponseEntity<Map<String, Object>> login(@RequestBody UserEntity userLogin) {
+        Map<String, Object> hasMap = new HashMap<>();
+        String text;
+
+        UserEntity user = this.userEntityService.findByUserEmail(userLogin.getEmail());
+        if (user != null) {
+            if (userLogin.getAccountPassword().equals(userLogin.getAccountPassword())) {
+                text = "Đăng nhập thành công";
+                hasMap.put("text", text);
+                hasMap.put("user1234567890", user);
+                return new ResponseEntity<>(hasMap, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
